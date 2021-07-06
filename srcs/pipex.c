f5815e423a66cd53f78af0278f956a2de186b942
jiaaren <jiaren.kjr@gmail.com>
2021-07-06 21:27:53 +0800

    working files

--- srcs/pipex.c
+++ srcs/pipex.c
@@ -6,7 +6,7 @@
 /*   By: jkhong <jkhong@student.42kl.edu.my>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/06 15:42:06 by jkhong            #+#    #+#             */
-/*   Updated: 2021/07/06 20:02:49 by jkhong           ###   ########.fr       */
+/*   Updated: 2021/07/06 21:27:09 by jkhong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,15 +45,32 @@
 	printf("fd %i fd2 %i fd[0] %i\n", fd, fd2, dup_fd[0]);
 */
 
+void	output_file(int fd)
+{
+	char c;
+	while (read(fd, &c, 1) > 0)
+		write(1, &c, 1); // 1 is write
+}
+
+void	write_file(char *filename, int fd)
+{
+	int		new_file;
+	char	c;
+
+	new_file = open(filename, O_CREAT | O_WRONLY | O_APPEND, 0777); // append for bonus
+	while (read(fd, &c, 1) > 0)
+		write(new_file, &c, 1);
+}
+
 int	read_file(char *filename, int dup_fd[2])
 {
 	int		fd;
-	char	c;
 	// initialising fd, closing the pipe and duplicating it to dup_fd via writing into [1]
 	fd = open(filename, O_RDONLY);
 	// only can dup2 if fd is within range, need to initialise it properly before, i.e. using pipe, cannot just declare an integer and duplicate it afterwards
 	dup2(fd, dup_fd[0]);
 	close(fd);
+	// char c;
 	// while (read(dup_fd[0], &c, 1) > 0)
 	// 	write(1, &c, 1); // 1 is write
 	return (0);
@@ -67,40 +84,69 @@ int	main(int argc, char *argv[])
 	int		pid;
 	char	**args;
 	char	*path;
-	int		fd; 		// for initial read, not required here because it will be closed off
-	int		dup_fd[2];
+	int		tmp_fd; 		// for initial read, not required here because it will be closed off
+	int		dup_fd_r[2];
+	int		dup_fd_w[2];
 
 	// forgot to pipe it previously
-	pipe(dup_fd);
-	read_file(argv[1], dup_fd);
+	pipe(dup_fd_r);
+	pipe(dup_fd_w);
+	read_file(argv[1], dup_fd_r);
 
 	i = 2; // 2 i.e. index of the first shell command
-	while (i < argc)
+	while (i < argc - 1) // -1 to consider last file as filename
 	{
-		pid = fork();
+		if (i != 2)
+		{
+			pipe(dup_fd_r);
+			pipe(dup_fd_w);
+			dup2(tmp_fd, dup_fd_r[0]);
+		}
 		args = ft_split(argv[i], ' ');
 		path = ft_strjoin("/bin/", args[0]);
+		pid = fork();
 		if (pid == 0) // i.e. child node
 		{
-			dup2(dup_fd[0], STDIN_FILENO); // change my dup_fd into STDIN
-			// after duplicating it, close unused port
-			close(dup_fd[0]);
 			// close unused ports for child, i.e. write
-			close(dup_fd[1]);
+			close(dup_fd_r[1]);
+			close(dup_fd_w[0]);
+
+			dup2(dup_fd_w[1], STDOUT_FILENO);
+			dup2(dup_fd_r[0], STDIN_FILENO); // change my dup_fd_r into STDIN
+			// after duplicating it, close unused port
+			close(dup_fd_r[0]);
+			close(dup_fd_w[1]);
 			execve(path, args, NULL);
 			// after execve, program will just stop
 			return (0);
 		}
+		// close unused
+		close(dup_fd_r[1]);
+		// close(dup_fd_w[0]);
+		// used
+		close(dup_fd_r[0]);
+		close(dup_fd_w[1]);
+		// output_file(dup_fd_w[0]);
+		tmp_fd = dup(dup_fd_w[0]);
+		close(dup_fd_w[0]);
 		// included waitpid here just in case execve runs finish before freeing below
 		// if waitpid is not added, the sequence of runs will be off, i.e. the data will be freed before all executions
 		// We can also actually just use wait() instead, since we are only dealing with 1 fork at a single time
-		// waitpid helps if there are multiple forks running at the same time, where sequencing is important for us.
-		close(dup_fd[0]);
-		close(dup_fd[1]);
+		// waitpid helps if there are multiple forks running at the same time, where sequencing is important for us.		
 		waitpid(pid, NULL, 0);
 		free_split(args);
 		free(path);
 		i++;
 	}
+	// be aware of how often we call write or read, the file pointer will always move and we need to pay close attention.
+	// output_file(tmp_fd); 
+	// write to file
+	write_file(argv[argc - 1], tmp_fd);
+	close(tmp_fd);
+	// char c;
+	// while (read(dup_fd[0], &c, 1) > 0)
+	// 	write(1, &c, 1); // 1 is write
 	return (0);
 }
+
+// we can close fd and the file?
